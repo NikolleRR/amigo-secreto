e13d23d204a78e20a1f782e5a81edeb777011803
Nikolle Romero <nikolleromero2003@gmail.com>
2025-08-11 17:32:13 -0500

    Actualizar codigo JS

--- app.js
+++ app.js
@@ -8,6 +8,17 @@ function agregarAmigo(){
     }else {
         amigos.push(nombresAmigos);
         limpiarCaja();
+        crearListaAmigos();
+    }
+}
+
+function crearListaAmigos(){
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    for(let i = 0; i < amigos.length; i++){
+        let li = document.createElement("li");
+        li.textContent = amigos[i];
+        lista.appendChild(li);
     }
 }
 
