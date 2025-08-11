3c8094921b6ef8ec36f3838e58f5a13c86983f89
Nikolle Romero <nikolleromero2003@gmail.com>
2025-08-11 16:14:59 -0500

    Actualizacion JS

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo(){
+    let nombresAmigos = document.getElementById("amigo");
+    if(nombresAmigos === ""){
+        alert("Por favor, inserte un nombre.");
+    }else {
+        amigos.push(nombresAmigos);
+    }
+}
+
+function limpiarCaja(){
+    document.getElementById('amigo').value = '';
+}
